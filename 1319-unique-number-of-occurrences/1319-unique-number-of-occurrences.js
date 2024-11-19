c8de5b039d1a92eaa03c4ec55825e8025be3cc12
Manav garg <78503190+gargmanav@users.noreply.github.com>
2024-11-20 01:20:49 +0530

    Time: 17 ms (5.02%), Space: 51.9 MB (5.42%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+   
+    const obj = {}
+   for(let char of arr){
+        obj[char] = (obj[char] || 0) + 1
+    }
+    console.log(obj)
+   const newarr =  Object.values(obj)
+   console.log(newarr)
+   const res = Array.from(new Set(newarr))
+   console.log(res)
+   if(res.length === newarr.length){
+       return true
+   }
+   return false
+};
