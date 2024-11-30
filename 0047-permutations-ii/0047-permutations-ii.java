b34e05a3aaf361da9912e63b6ec3f372a681c597
Manav garg <78503190+gargmanav@users.noreply.github.com>
2024-12-01 01:15:57 +0530

    Time: 2 ms (67.13%), Space: 44.9 MB (35.45%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,16 +1,18 @@
 class Solution {
     public void performRecur(List<List<Integer>> ans,List<Integer> ds,boolean[] freq,int[] nums){
-        if(ds.size() == nums.length && !ans.contains(ds)){
+        if(ds.size() == nums.length){
             ans.add(new ArrayList<>(ds));
             return;
         }
         for(int i = 0;i<nums.length;i++){
-            if(freq[i])continue;
+            if(!freq[i]){
+                if(i > 0 && nums[i] == nums[i-1] && !freq[i - 1])continue;
                 freq[i] = true;
                 ds.add(nums[i]);
                 performRecur(ans,ds,freq,nums);
                 ds.remove(ds.size()-1);
                 freq[i] = false;
+            }
         }
     }
     
