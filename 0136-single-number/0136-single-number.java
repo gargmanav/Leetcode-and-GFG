84aac8e057b04dfc77735f2bba5b1c47ada5f3da
Manav garg <78503190+gargmanav@users.noreply.github.com>
2024-12-18 22:45:27 +0530

    Time: 1 ms (99.87%), Space: 46.1 MB (45.28%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,12 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        if(nums.length == 1)return nums[0];
-        Arrays.sort(nums);
-        for(int i = 1;i<nums.length;i+=2){
-            if(nums[i] != nums[i-1]){
-                return nums[i-1];
-            }
+        int xor = 0;
+        for(int num : nums){
+         xor ^= num;
         }
-        return nums[nums.length - 1];
+        return xor;
     }
 }
