f77756d3500a826a546ad4e2a796b8546559d23c
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-03-23 00:44:06 +0530

    Time: 52 ms (11.08%), Space: 44.7 MB (92.03%) - LeetHub

---
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,24 @@
+class Solution {
+    private void dfs(int[][] stones,boolean[] visited,int index){
+        visited[index] = true;
+        for(int i = 0;i<stones.length;i++){
+            int r = stones[index][0];
+            int c = stones[index][1];
+            if(visited[i] == false && (stones[i][0] == r || stones[i][1] == c)){
+                dfs(stones,visited,i);
+            }
+        }
+    }
+    public int removeStones(int[][] stones) {
+        int n = stones.length;
+        int group = 0;
+        boolean[] visited = new boolean[n];
+        for(int i = 0;i<n;i++){
+            if(visited[i] == false){
+                dfs(stones,visited,i);
+                group++;
+            }
+        }
+        return n-group;
+    }
+}
