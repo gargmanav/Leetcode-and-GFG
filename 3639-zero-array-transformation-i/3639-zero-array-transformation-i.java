a732d135269072583c14c89a1241cc3bf48145eb
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-05-21 00:55:05 +0530

    Time: 4 ms (13.33%), Space: 82.2 MB (84.05%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
@@ -1,27 +1,25 @@
 class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
         int n = nums.length;
-        int[] diff = new int[n + 1]; // extra space to handle j+1
-
-        for (int[] query : queries) {
-            int i = query[0];
-            int j = query[1];
-            if (i < 0 || j >= n || i > j) return false;
-
-            diff[i] -= 1;
-            if (j + 1 < n) {
-                diff[j + 1] += 1;
+        int[] diff = new int[n + 1];
+        
+        for(int[] arr : queries){
+            int a = arr[0];
+            int b = arr[1];
+            if (a < 0 || b >= n || a > b) return false;
+            diff[a] -= 1;
+            if(b + 1 < n){
+                diff[b + 1] += 1;
             }
         }
-
-        // Apply prefix sum and update nums
         int sum = 0;
-        for (int i = 0; i < n; i++) {
+        for(int i = 0;i<n;i++){
             sum += diff[i];
             nums[i] += sum;
-            if (nums[i] > 0) return false; // check if not zero
+            if(nums[i] > 0){
+                return false;
+            }
         }
-
         return true;
     }
 }
