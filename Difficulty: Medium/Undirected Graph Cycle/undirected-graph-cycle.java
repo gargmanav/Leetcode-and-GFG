0154a4cbaa9671581e58b4d95c311e2b8f0e4911
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-05-28 11:18:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,62 +1,33 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            String[] s = br.readLine().trim().split(" ");
-            int V = Integer.parseInt(s[0]);
-            int E = Integer.parseInt(s[1]);
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
-            for (int i = 0; i < E; i++) {
-                String[] S = br.readLine().trim().split(" ");
-                int u = Integer.parseInt(S[0]);
-                int v = Integer.parseInt(S[1]);
-                adj.get(u).add(v);
-                adj.get(v).add(u);
-            }
-            Solution obj = new Solution();
-            boolean ans = obj.isCycle(adj);
-            if (ans)
-                System.out.println("1");
-            else
-                System.out.println("0");
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
-    
-    public boolean dfs(ArrayList<ArrayList<Integer>> adj,boolean[] visited,int u,int parent){
-        visited[u] = true;
-        for(int v : adj.get(u)){
-            if(v == parent) continue;
-            if(visited[v] == true){
-                return true;
-            }
-            if(dfs(adj,visited,v,u) == true){
+    private boolean dfs(int curr,int parent,boolean[] visited,List<List<Integer>> adj){
+        visited[curr] = true;
+        for (int neighbor : adj.get(curr)) {
+            if (!visited[neighbor]) {
+                if (dfs(neighbor, curr, visited, adj)) {
+                    return true;
+                }
+            } else if (neighbor != parent) {
                 return true;
             }
         }
         return false;
     }
-    
-    public boolean isCycle(ArrayList<ArrayList<Integer>> adj) {
-        boolean[] visited = new boolean[adj.size()];
-        for(int i = 0;i<adj.size();i++){
-            if(visited[i] == false && dfs(adj,visited,i,-1)){
-                return true;
-            }
-        }
-        return false;
+    public boolean isCycle(int V, int[][] edges) {
+       List<List<Integer>> adj = new ArrayList<>();
+       for(int i = 0;i<V;i++)adj.add(new ArrayList<>());
+       for(int[] edge : edges){
+           adj.get(edge[0]).add(edge[1]);
+           adj.get(edge[1]).add(edge[0]);
+       }
+       boolean[] visited = new boolean[V];
+       Arrays.fill(visited,false);
+       for(int i=0;i<V;i++){
+           if(!visited[i]){
+               if(dfs(i,-1,visited,adj)){
+                  return true;
+               }
+           }
+       } 
+       return false;
     }
 }
