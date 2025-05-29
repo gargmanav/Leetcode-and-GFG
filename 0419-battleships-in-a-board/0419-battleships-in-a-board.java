3583c3b9ebd1f37e24f0f48359d94176d5da1623
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-05-29 09:56:20 +0530

    Time: 1 ms (94.66%), Space: 45.2 MB (91.02%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.java
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.java
@@ -1,23 +1,16 @@
 class Solution {
     public int countBattleships(char[][] board) {
-      int count = 0;
-        int rows = board.length;
-        int cols = board[0].length;
+        int count = 0;
+        int n = board.length,m = board[0].length;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(board[i][j] == '.')continue;
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                // Skip empty water
-                if (board[i][j] == '.') continue;
-
-                // Skip if it's a continuation of a ship from above or left
-                if (i > 0 && board[i - 1][j] == 'X') continue;
-                if (j > 0 && board[i][j - 1] == 'X') continue;
-
-                // It's the start of a new ship
+                if(i > 0 && board[i - 1][j] == 'X')continue;
+                if(j > 0 && board[i][j - 1] == 'X')continue;
                 count++;
             }
         }
-
-        return count;  
+        return count;
     }
 }
