a22d05916cc72677ba5150a84cab8607b0eea433
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-06-14 02:26:45 +0530

    Time: 0 ms (100%), Space: 47.8 MB (55.54%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,36 +1,25 @@
 class Solution {
-    private void dfs(ArrayList<ArrayList<Integer>> adj,boolean[] visited,int index){
-      visited[index] = true;
-
-      for(int v : adj.get(index)){
-        if(visited[v] == false){
-            dfs(adj,visited,v);
-        }
-      }
+    private void dfs(int curr,int[][] isConnected,boolean[] visited){
+       if(visited[curr]){
+        return;
+       }
+       visited[curr] = true;
+       for(int i = 0;i<isConnected[curr].length;i++){
+          if(isConnected[curr][i] == 1 && !visited[i]){
+            dfs(i,isConnected,visited);
+          }
+       }
     }
     public int findCircleNum(int[][] isConnected) {
+        int count = 0;
         int n = isConnected.length;
-    ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-    
-    for (int i = 0; i < n; i++) {
-        adj.add(new ArrayList<>());
-    }
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (isConnected[i][j] == 1 && i != j) {
-                adj.get(i).add(j);
+        boolean[] visited = new boolean[n];
+        for(int i = 0;i<n;i++){
+            if(!visited[i]){
+                dfs(i,isConnected,visited);
+                count++;
             }
         }
-    }
-    boolean[] visited = new boolean[adj.size()];
-    int count = 0;
-    for(int i = 0;i<adj.size();i++){
-        if(visited[i] == false){
-            dfs(adj,visited,i);
-            count++;
-        }
-    }
-    return count;
+        return count;
     }
 }
