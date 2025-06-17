a27468fd7e05ff08e3273b311315073f6d38fbcb
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-06-18 01:55:23 +0530

    Time: 204 ms (23.83%), Space: 41.2 MB (97.88%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,44 +1,42 @@
 class Solution {
-    public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
+    private int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (dfs(board, word, i, j, 0)) {
-                    return true;
+    private boolean dfs(int i, int j, String word, int index, char[][] board, boolean[][] visited) {
+        if (index == word.length()) return true;
+        
+        int n = board.length, m = board[0].length;
+        
+        if (i < 0 || j < 0 || i >= n || j >= m || visited[i][j] || board[i][j] != word.charAt(index)) {
+            return false;
+        }
+
+        visited[i][j] = true;
+
+        for (int[] dir : directions) {
+            int ni = i + dir[0], nj = j + dir[1];
+            if (dfs(ni, nj, word, index + 1, board, visited)) {
+                return true;
+            }
+        }
+
+        visited[i][j] = false; // backtrack
+        return false;
+    }
+
+    public boolean exist(char[][] board, String word) {
+        int n = board.length, m = board[0].length;
+        boolean[][] visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    if (dfs(i, j, word, 0, board, visited)) {
+                        return true;
+                    }
                 }
             }
         }
 
         return false;
-    }
-
-    private boolean dfs(char[][] board, String word, int i, int j, int idx) {
-        // base case: word is completely matched
-        if (idx == word.length()) {
-            return true;
-        }
-
-        // boundary check and character match check
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length ||
-            board[i][j] != word.charAt(idx)) {
-            return false;
-        }
-
-        // mark visited
-        char temp = board[i][j];
-        board[i][j] = '#';
-
-        // explore all 4 directions
-        boolean found = dfs(board, word, i + 1, j, idx + 1) ||
-                        dfs(board, word, i - 1, j, idx + 1) ||
-                        dfs(board, word, i, j + 1, idx + 1) ||
-                        dfs(board, word, i, j - 1, idx + 1);
-
-        // backtrack
-        board[i][j] = temp;
-
-        return found;
     }
 }
