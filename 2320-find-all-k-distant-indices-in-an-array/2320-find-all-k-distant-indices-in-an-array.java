f6c4a770195cc310899bd2ff255bdbc412c59938
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-06-25 00:15:41 +0530

    Time: 5 ms (79.06%), Space: 45.1 MB (38.84%) - LeetHub

--- 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.java
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.java
@@ -1,22 +1,25 @@
 class Solution {
     public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
-        List<Integer> res = new ArrayList<>();
-        List<Integer> keys = new ArrayList<>();
-        int n = nums.length;
-        for(int i = 0;i<n;i++){
-            if(nums[i] == key){
-                keys.add(i);
-            }
-        }
-        for(int i = 0;i<n;i++){
-            for(int j : keys){
-                int num = Math.abs(i - j);
-                if(num <= k){
-                    res.add(i);
-                    break;
+         int n = nums.length;
+        boolean[] mark = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == key) {
+                int start = Math.max(0, i - k);
+                int end = Math.min(n - 1, i + k);
+                for (int j = start; j <= end; j++) {
+                    mark[j] = true;
                 }
             }
         }
+
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (mark[i]) {
+                res.add(i);
+            }
+        }
+
         return res;
     }
 }
