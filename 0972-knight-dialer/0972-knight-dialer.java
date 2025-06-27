b196c71284d39a3026dc10085987f910fcd36514
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-06-28 02:39:53 +0530

    Time: 39 ms (48.71%), Space: 44.7 MB (63.1%) - LeetHub

--- 0972-knight-dialer/0972-knight-dialer.java
+++ 0972-knight-dialer/0972-knight-dialer.java
@@ -1,39 +1,39 @@
 class Solution {
+
+    int[][] dp;
+    int[][] moves = {
+        {4,6},    // 0
+        {6,8},    // 1
+        {7,9},    // 2
+        {4,8},    // 3
+        {0,3,9},  // 4
+        {},       // 5 -> no move
+        {0,1,7},  // 6
+        {2,6},    // 7
+        {1,3},    // 8
+        {2,4}     // 9
+    };
     public int knightDialer(int n) {
-        int mod = 1_000_000_007;
-
-        int[][] moves = {
-            {4,6},    // 0
-            {6,8},    // 1
-            {7,9},    // 2
-            {4,8},    // 3
-            {3,9,0},  // 4
-            {},       // 5 (invalid)
-            {1,7,0},  // 6
-            {2,6},    // 7
-            {1,3},    // 8
-            {2,4}     // 9
-        };
-
-        long[] prev = new long[10];
-        // Base case: for length = 1, every digit has count 1
-        Arrays.fill(prev, 1);
-
-        for (int i = 2; i <= n; i++) {
-            long[] curr = new long[10];
-            for (int digit = 0; digit <= 9; digit++) {
-                for (int nei : moves[digit]) {
-                    curr[digit] = (curr[digit] + prev[nei]) % mod;
-                }
-            }
-            prev = curr;
+        dp = new int[n+1][10];
+        for(int[] arr : dp){
+            Arrays.fill(arr,-1);
         }
-
-        long result = 0;
-        for (long val : prev) {
-            result = (result + val) % mod;
+        int count = 0;
+        for(int i = 0;i < moves.length;i++){
+            count = (count + dfs(n - 1,i)) % 1000000007;
         }
-
-        return (int) result;
+        return count;
+    }
+    private int dfs(int leftsum,int U){
+        if(leftsum == 0){
+           return 1;
+        }
+        if(dp[leftsum][U] != -1)return dp[leftsum][U];
+        int res = 0;
+        for(int v : moves[U]){
+          res = (res + dfs(leftsum - 1,v)) % 1000000007;
+        }
+        dp[leftsum][U] = res;
+        return dp[leftsum][U];
     }
 }
