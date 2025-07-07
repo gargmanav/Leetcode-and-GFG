229ff1954d6d87e3a3594ecee2ee8f2a3d4bccae
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-07-08 01:23:47 +0530

    Time: 63 ms (95.35%), Space: 75.7 MB (43.44%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.java
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.java
@@ -1,39 +1,33 @@
-import java.util.*;
-
 class Solution {
     public int maxEvents(int[][] events) {
-        Arrays.sort(events, (a, b) -> a[0] - b[0]); // sort by start day
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>(); // stores end days
+        Arrays.sort(events,(a,b)->a[0] - b[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int n = events.length;
+        int day = events[0][0];
+        int i = 0;
+        int count = 0;
 
-        int i = 0, day = 1, n = events.length, attended = 0;
-
-        // Get last day to loop until that
         int maxDay = 0;
-        for (int[] event : events) {
-            maxDay = Math.max(maxDay, event[1]);
+        for(int[] event : events){
+            maxDay = Math.max(maxDay,event[1]);
         }
 
-        while (day <= maxDay) {
-            // Add events that start today
-            while (i < n && events[i][0] == day) {
-                minHeap.offer(events[i][1]);
+        while(day <= maxDay){
+            while(i < n && events[i][0] == day){
+                pq.add(events[i][1]);
                 i++;
             }
 
-            // Remove events that already ended before today
-            while (!minHeap.isEmpty() && minHeap.peek() < day) {
-                minHeap.poll();
+            while(!pq.isEmpty() && pq.peek() < day){
+                pq.poll();
             }
 
-            // Attend event with earliest end day
-            if (!minHeap.isEmpty()) {
-                minHeap.poll();
-                attended++;
+            if(!pq.isEmpty()){
+                pq.poll();
+                count++;
             }
-
             day++;
         }
-
-        return attended;
+        return count;
     }
 }
