00d988431ccd50d0d9c6e2cafbb8e8f926dfb6df
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-07-08 03:04:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.java
+++ Difficulty: Medium/Topological sort/topological-sort.java
@@ -1,86 +1,39 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-
-        while (t-- > 0) {
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-            int vertices = Integer.parseInt(read.readLine());
-            int edges = Integer.parseInt(read.readLine());
-
-            for (int i = 0; i < vertices; i++) adj.add(i, new ArrayList<Integer>());
-
-            int p = 0;
-            for (int i = 1; i <= edges; i++) {
-                String s[] = read.readLine().trim().split("\\s+");
-                int u = Integer.parseInt(s[0]);
-                int v = Integer.parseInt(s[1]);
-                adj.get(u).add(v);
-            }
-
-            ArrayList<Integer> res = new Solution().topologicalSort(adj);
-
-            if (check(adj, vertices, res) == true)
-                System.out.println("1");
-            else
-                System.out.println("0");
-            System.out.println("~");
-        }
-    }
-
-    static boolean check(ArrayList<ArrayList<Integer>> adj, int V,
-                         ArrayList<Integer> res) {
-
-        if (V != res.size()) return false;
-
-        int[] map = new int[V];
-        for (int i = 0; i < V; i++) {
-            map[res.get(i)] = i;
-        }
-        for (int i = 0; i < V; i++) {
-            for (int v : adj.get(i)) {
-                if (map[i] > map[v]) return false;
-            }
-        }
-        return true;
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-    // Function to return list containing vertices in Topological order.
-    static ArrayList<Integer> topologicalSort(ArrayList<ArrayList<Integer>> adj) {
-        int n = adj.size();
-        Queue<Integer> que = new LinkedList<>();
-        ArrayList<Integer> result = new ArrayList<>();
-        int[] indegree = new int[n];
-        for(int u = 0;u<n;u++){
-            for(int v : adj.get(u)){
-                indegree[v] = indegree[v] + 1;
-            }
-        }
-        for(int i = 0;i<n;i++){
-            if(indegree[i] == 0){
-                que.add(i);
-            }
-        }
-        while(!que.isEmpty()){
-            int u = que.poll();
-            result.add(u);
-            for(int v : adj.get(u)){
-                indegree[v] = indegree[v] - 1;
-                if(indegree[v] == 0){
-                    que.add(v);
-                }
-            }
-        }
-        return result;
+    public static ArrayList<Integer> topoSort(int V, int[][] edges) {
+       List<List<Integer>> adj = new ArrayList<>();
+       
+       for(int i = 0;i < V;i++){
+           adj.add(new ArrayList<>());
+       }
+       for(int[] edge : edges){
+           adj.get(edge[0]).add(edge[1]);
+       }
+       
+       int[] indegree = new int[V];
+       for(int i = 0;i < V;i++){
+           for(int v : adj.get(i)){
+               indegree[v]++;
+           }
+       }
+       
+       Queue<Integer> que = new LinkedList<>();
+       for(int i = 0;i < V;i++){
+           if(indegree[i] == 0){
+               que.add(i);
+           }
+       }
+       
+       ArrayList<Integer> result = new ArrayList<>();
+       while(!que.isEmpty()){
+           int u = que.poll();
+           result.add(u);
+           for(int v : adj.get(u)){
+               indegree[v]--;
+               if(indegree[v] == 0){
+                   que.add(v);
+               }
+           }
+       }
+       return result;
     }
 }
