f621fb849c733ba1a4cf8ab64a3cd5be515f4595
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-07-12 02:17:16 +0530

    Time: 69 ms (80.86%), Space: 58.5 MB (23.4%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.java
+++ 0883-car-fleet/0883-car-fleet.java
@@ -1,31 +1,27 @@
 class Solution {
+    class Pair{
+        int pos;
+        float time;
+        Pair(int pos,float time){
+           this.pos = pos;
+           this.time = time;
+        }
+    }
     public int carFleet(int target, int[] position, int[] speed) {
         int n = position.length;
-        if (n == 0) return 0;
-
-        // Step 1: Pair up position and speed
-        double[][] cars = new double[n][2];
-        for (int i = 0; i < n; i++) {
-            cars[i][0] = position[i];
-            cars[i][1] = (double)(target - position[i]) / speed[i]; // time = distance / speed
+        Pair[] pair = new Pair[n];
+        for(int i = 0;i<n;i++){
+            pair[i] = new Pair(position[i], (float)(target - position[i]) / (float)speed[i]);
         }
-
-        // Step 2: Sort by position descending (farthest from target first)
-        Arrays.sort(cars, (a, b) -> Double.compare(b[0], a[0]));
-
-        // Step 3: Count fleets using stack-style approach
-        int fleets = 0;
-        double prevTime = 0.0;
-
-        for (int i = 0; i < n; i++) {
-            double currTime = cars[i][1];
-            if (currTime > prevTime) {
-                fleets++;
-                prevTime = currTime;
+        Stack<Float> st = new Stack<>();
+        Arrays.sort(pair,(a,b)->b.pos - a.pos);
+        for(int i = 0;i<n;i++){
+            float time = pair[i].time;
+            while(st.isEmpty() || time > st.peek()){
+                st.add(time);
             }
-            // else: car joins fleet ahead of it
+            
         }
-
-        return fleets;
+        return st.size();
     }
 }
