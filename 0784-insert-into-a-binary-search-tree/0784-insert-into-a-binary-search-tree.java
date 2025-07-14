f7fbcab45d68b620e63dcc4d73e1f12626c584dc
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-07-15 01:06:13 +0530

    Time: 0 ms (100%), Space: 45.4 MB (39.64%) - LeetHub

--- 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.java
+++ 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.java
@@ -14,16 +14,14 @@
  * }
  */
 class Solution {
-    
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        if(root == null){
-            return new TreeNode(val);
-        }
-        if(val < root.val){
-            root.left = insertIntoBST(root.left,val);
-        }else if(val > root.val){
-            root.right = insertIntoBST(root.right,val);
-        }
+       if(root == null){
+         return new TreeNode(val);
+       }
+        if(root.val > val ){
+           root.left = insertIntoBST(root.left,val);
+        }else root.right = insertIntoBST(root.right,val);
         return root;
     }
+   
 }
