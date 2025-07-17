1f897015275e7e7736b6a9e39ec569e8c0110b5a
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-07-18 00:50:02 +0530

    Time: 18 ms (4.94%), Space: 45.8 MB (13.84%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
@@ -1,16 +1,26 @@
 class Solution {
+    private void swap(int a,int b,int[] sarr){
+        int temp = sarr[a];
+        sarr[a] = sarr[b];
+        sarr[b] = temp;
+    }
     public long minimumSteps(String s) {
         long steps = 0;
-        long whiteCount = 0;
-
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) == '0') {
-                whiteCount++;
-            } else { // it's a black ball
-                steps += whiteCount;
+        int[] arr = s.chars().map(c -> c - '0').toArray();
+        int i = 0,j = 1;
+        while(j < arr.length){
+            if(arr[i] == 1 && arr[j] == 0){
+                steps += j - i;
+                swap(i,j,arr);
+                i++;
+                j++;
+            }else{
+                while(i < j && arr[i] != 1){
+                    i++;
+                }
+                j++;
             }
         }
-
         return steps;
     }
 }
