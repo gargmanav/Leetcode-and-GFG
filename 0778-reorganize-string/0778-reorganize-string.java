7934cfb7e46a9b291a69e0425ce6d98f4b7b0193
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-07-19 01:04:41 +0530

    Time: 4 ms (50.45%), Space: 41.9 MB (45.94%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.java
+++ 0778-reorganize-string/0778-reorganize-string.java
@@ -1,36 +1,42 @@
-import java.util.*;
-
 class Solution {
+    
+    class Pair{
+        char ch;
+        int freq;
+        Pair(char ch,int freq){
+           this.ch = ch;
+           this.freq = freq; 
+        }
+    }
     public String reorganizeString(String s) {
-        int[] count = new int[26];
-        for (char c : s.toCharArray()) {
-            count[c - 'a']++;
+        Map<Character,Integer> map = new HashMap<>();
+        for(char c : s.toCharArray()){
+           map.put(c,map.getOrDefault(c,0) + 1);
         }
-
-        PriorityQueue<Character> maxHeap = new PriorityQueue<>((a, b) -> count[b - 'a'] - count[a - 'a']);
-        for (char c = 'a'; c <= 'z'; c++) {
-            if (count[c - 'a'] > 0) {
-                if (count[c - 'a'] > (s.length() + 1) / 2) return ""; // impossible
-                maxHeap.add(c);
-            }
+        PriorityQueue<Pair> pq = new PriorityQueue<>(
+            (p1, p2) -> p2.freq - p1.freq        // descending order
+        );
+        for(Map.Entry<Character,Integer> e : map.entrySet()){
+            pq.add(new Pair(e.getKey(),e.getValue()));
+            if(e.getValue() > (s.length() + 1 )/2)return "";
         }
+        
+        StringBuilder str = new StringBuilder();
+        while(pq.size() >= 2){
+            Pair max = pq.poll();
+            Pair curr = pq.poll();
+            
+            str.append(max.ch);
+            str.append(curr.ch);
 
-        StringBuilder result = new StringBuilder();
-        while (maxHeap.size() >= 2) {
-            char first = maxHeap.poll();
-            char second = maxHeap.poll();
-
-            result.append(first);
-            result.append(second);
-            count[first - 'a']--;
-            count[second - 'a']--;
-
-            if (count[first - 'a'] > 0) maxHeap.add(first);
-            if (count[second - 'a'] > 0) maxHeap.add(second);
+            if(--max.freq > 0)pq.add(max);
+            if(--curr.freq > 0)pq.add(curr);
         }
-
-        if (!maxHeap.isEmpty()) result.append(maxHeap.poll()); // last character if any
-
-        return result.toString();
+        if(!pq.isEmpty()){
+            Pair curr = pq.poll();
+            if(curr.freq >=2)return "";
+            str.append(curr.ch);
+        }
+        return str.toString();
     }
 }
