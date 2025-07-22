6a32c4ed02dd5c30ba3ba320086b8a017930f55e
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-07-23 02:57:02 +0530

    Time: 10 ms (58.41%), Space: 45 MB (22.78%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,25 +1,27 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
-        int sum = 0;
-        for (int num : nums) sum += num;
-        
-        // Check if it's possible to partition
-        if ((sum + target) % 2 != 0 || sum < Math.abs(target)) return 0;
-        
-        int subsetSum = (sum + target) / 2;
-        return countSubsets(nums, subsetSum);
-    }
-    
-    private int countSubsets(int[] nums, int sum) {
-        int[] dp = new int[sum + 1];
-        dp[0] = 1;
-        
-        for (int num : nums) {
-            for (int j = sum; j >= num; j--) {
-                dp[j] += dp[j - num];
-            }
+    int[][] memo;
+    private int findways(int[] nums, int target,int index,int currSum){
+        if(index == nums.length){
+            if(currSum == target){
+            return 1;
         }
+            return 0;
+        }
+        if(memo[index][currSum + 1000] != -1){
+            return memo[index][currSum + 1000];
+        }
+        int adding = findways(nums,target,index + 1,currSum + nums[index]);
+        int minus = findways(nums,target,index + 1,currSum - nums[index]);
         
-        return dp[sum];
+        memo[index][currSum + 1000] = adding + minus;
+        return memo[index][currSum + 1000];
+    }
+    public int findTargetSumWays(int[] nums, int target) {
+        int n = nums.length;
+        memo = new int[nums.length][2001];
+        for(int[] mem : memo){
+            Arrays.fill(mem,-1);
+        }
+        return findways(nums,target,0,0);
     }
 }
