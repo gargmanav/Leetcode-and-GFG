2b1fd2e30c8d8144e3193913bff13016691221f7
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-08-12 02:21:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3973-flip-square-submatrix-vertically/3973-flip-square-submatrix-vertically.java
+++ 3973-flip-square-submatrix-vertically/3973-flip-square-submatrix-vertically.java
@@ -1,14 +1,13 @@
 class Solution {
     public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
-        // Reverse rows inside the k×k submatrix
-        for (int i = 0; i < k / 2; i++) {
-            int r1 = x + i;
-            int r2 = x + (k - 1 - i);
-            for (int col = y; col < y + k; col++) {
-                int temp = grid[r1][col];
-                grid[r1][col] = grid[r2][col];
-                grid[r2][col] = temp;
-            }
+        int a = k + 1;
+        for(int i = 0;i < k/2; i++){
+            a--;
+           for(int j = 0;j<k;j++){
+              int temp = grid[x + i][y + j];
+              grid[x + i][y + j] = grid[x + i + a - 1][y + j];
+              grid[x + i + a - 1][y + j] = temp;
+           }
         }
         return grid;
     }
