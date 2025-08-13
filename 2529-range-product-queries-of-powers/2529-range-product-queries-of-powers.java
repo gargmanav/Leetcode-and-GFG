c15b5182f8d62de481218baca329fa9ab6cf6304
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-08-14 01:02:49 +0530

    Time: 21 ms (35.95%), Space: 93.3 MB (24.83%) - LeetHub

--- 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.java
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.java
@@ -1,26 +1,22 @@
 class Solution {
+    private int modulo = 1_000_000_007;
     public int[] productQueries(int n, int[][] queries) {
-        final int MOD = 1_000_000_007;
-
-        // Step 1: Extract powers of two from n
-        java.util.List<Integer> powers = new java.util.ArrayList<>();
-        for (int i = 0; i < 32; i++) {
-            if (((n >> i) & 1) == 1) {
-                powers.add(1 << i);
+        int m = queries.length;
+        int[] result = new int[m];
+        List<Integer> power = new ArrayList<>();
+        for(int i = 0;i<32;i++){
+            if(((n >> i) & 1) == 1){
+                power.add(1 << i);
             }
         }
-
-        // Step 2: Answer each query
-        int q = queries.length;
-        int[] ans = new int[q];
-        for (int i = 0; i < q; i++) {
-            long prod = 1;
-            for (int j = queries[i][0]; j <= queries[i][1]; j++) {
-                prod = (prod * powers.get(j)) % MOD;
+        int j = 0;
+        for(int[] query : queries){
+            long product = 1;
+            for(int i = query[0];i <= query[1];i++){
+               product = (product * power.get(i) % modulo);
             }
-            ans[i] = (int) prod;
+            result[j++] = (int) product;
         }
-
-        return ans;
+        return result;
     }
 }
