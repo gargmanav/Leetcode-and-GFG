223ae6573004e6cb4bb0499d3a828a02605da552
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-08-15 23:28:36 +0530

    Time: 27 ms (49.37%), Space: 84.5 MB (52.16%) - LeetHub

--- 2035-count-sub-islands/2035-count-sub-islands.java
+++ 2035-count-sub-islands/2035-count-sub-islands.java
@@ -1,34 +1,29 @@
 class Solution {
+    private int[][] directions = {{0,1},{1,0},{0,-1},{-1,0}};
+    int m,n;
+    private boolean dfs(int i,int j,int[][] grid1, int[][] grid2){
+      if(i < 0 || j < 0 || i >= m || j >= n || grid2[i][j] == 0)return true;
+      boolean result = true;
+      if(grid1[i][j] == 0) result = false;
+      grid2[i][j] = 0;
+      for(int[] dir : directions){
+        if(!dfs(dir[0] + i,dir[1] + j,grid1,grid2))result = false;
+      }
+      return result;
+    }
     public int countSubIslands(int[][] grid1, int[][] grid2) {
-        int m = grid1.length, n = grid1[0].length;
+        m = grid1.length;
+        n = grid1[0].length;
         int count = 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid2[i][j] == 1) {
-                    if (dfs(grid1, grid2, i, j, m, n)) {
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(grid2[i][j] == 1){
+                    if(dfs(i,j,grid1,grid2)){
                         count++;
                     }
                 }
             }
         }
         return count;
-    }
-
-    private boolean dfs(int[][] grid1, int[][] grid2, int i, int j, int m, int n) {
-        if (i < 0 || i >= m || j < 0 || j >= n || grid2[i][j] == 0) {
-            return true; // base case: no violation here
-        }
-
-        grid2[i][j] = 0; // mark visited
-
-        boolean isSub = grid1[i][j] == 1; // this cell must be land in grid1
-
-        boolean up    = dfs(grid1, grid2, i - 1, j, m, n);
-        boolean down  = dfs(grid1, grid2, i + 1, j, m, n);
-        boolean left  = dfs(grid1, grid2, i, j - 1, m, n);
-        boolean right = dfs(grid1, grid2, i, j + 1, m, n);
-
-        return isSub && up && down && left && right;
     }
 }
