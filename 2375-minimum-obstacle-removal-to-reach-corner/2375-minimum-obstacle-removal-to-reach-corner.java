6121c7acd95a1251f89db6e91a10b863bbde8d31
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-08-18 01:58:31 +0530

    Time: 137 ms (48.83%), Space: 83.2 MB (14.03%) - LeetHub

--- 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.java
+++ 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.java
@@ -1,37 +1,29 @@
-import java.util.*;
-
 class Solution {
+
     private int[][] directions = {{0,1},{1,0},{0,-1},{-1,0}};
-    
     public int minimumObstacles(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
+        int m = grid.length;
+        int n = grid[0].length;
         int[][] dist = new int[m][n];
-        for (int[] row : dist) Arrays.fill(row, Integer.MAX_VALUE);
-        
-        Deque<int[]> dq = new ArrayDeque<>();
-        dq.offer(new int[]{0, 0});
-        dist[0][0] = 0;
-        
-        while (!dq.isEmpty()) {
-            int[] cur = dq.pollFirst();
-            int x = cur[0], y = cur[1];
-            
-            for (int[] d : directions) {
-                int nx = x + d[0], ny = y + d[1];
-                if (nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
-                
-                int newCost = dist[x][y] + grid[nx][ny];
-                if (newCost < dist[nx][ny]) {
-                    dist[nx][ny] = newCost;
-                    if (grid[nx][ny] == 1) {
-                        dq.offerLast(new int[]{nx, ny}); // obstacle → back
-                    } else {
-                        dq.offerFirst(new int[]{nx, ny}); // free → front
-                    }
+        for(int[] arr : dist){
+            Arrays.fill(arr,Integer.MAX_VALUE);
+        }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[0] - b[0]);
+    pq.add(new int[]{0,0,0});
+    while(!pq.isEmpty()){
+        int[] curr = pq.poll();
+        int count = curr[0],row = curr[1],col = curr[2];
+        if(row == m - 1 && col == n - 1)return count;
+        for(int[] dir : directions){
+            int nrow = row + dir[0],ncol = col + dir[1];
+            if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n){
+                if(dist[nrow][ncol] > count + grid[nrow][ncol]){
+                    dist[nrow][ncol] = count + grid[nrow][ncol];
+                    pq.add(new int[]{count + grid[nrow][ncol],nrow,ncol});
                 }
             }
         }
-        
-        return dist[m-1][n-1];
+    }
+    return 0;
     }
 }
