ebd9bd9ab6d2b7cad695c826dd739cd7e5a38c8b
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-08-19 02:20:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,33 +1,33 @@
 class Solution {
-    private boolean dfs(int curr,int parent,boolean[] visited,List<List<Integer>> adj){
-        visited[curr] = true;
-        for (int neighbor : adj.get(curr)) {
-            if (!visited[neighbor]) {
-                if (dfs(neighbor, curr, visited, adj)) {
-                    return true;
-                }
-            } else if (neighbor != parent) {
+    private boolean dfs(List<List<Integer>> adj,boolean[] visited,int U,int parent){
+        visited[U] = true;
+        for(int v : adj.get(U)){
+            if(parent == v)continue;
+            if(visited[v]){
+                return true;
+            }
+            if(dfs(adj,visited,v,U) == true){
                 return true;
             }
         }
         return false;
     }
     public boolean isCycle(int V, int[][] edges) {
-       List<List<Integer>> adj = new ArrayList<>();
-       for(int i = 0;i<V;i++)adj.add(new ArrayList<>());
-       for(int[] edge : edges){
-           adj.get(edge[0]).add(edge[1]);
-           adj.get(edge[1]).add(edge[0]);
-       }
-       boolean[] visited = new boolean[V];
-       Arrays.fill(visited,false);
-       for(int i=0;i<V;i++){
-           if(!visited[i]){
-               if(dfs(i,-1,visited,adj)){
-                  return true;
-               }
-           }
-       } 
-       return false;
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0;i<V;i++)adj.add(new ArrayList<>());
+        for(int[] arr : edges){
+            adj.get(arr[0]).add(arr[1]);
+            adj.get(arr[1]).add(arr[0]);
+        }
+        boolean[] visited = new boolean[V];
+        boolean ans = false;
+        for(int i = 0;i<V;i++){
+            if(!visited[i]){
+                if(dfs(adj,visited,i,-1) == true){
+                    ans = true;
+                }
+            }
+        }
+        return ans;
     }
 }
