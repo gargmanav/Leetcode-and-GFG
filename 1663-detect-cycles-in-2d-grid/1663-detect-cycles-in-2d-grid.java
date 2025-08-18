9bda73e188fff782e71d1309c3527472078a1799
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-08-19 03:04:47 +0530

    Time: 18 ms (83.73%), Space: 98.8 MB (24.28%) - LeetHub

--- 1663-detect-cycles-in-2d-grid/1663-detect-cycles-in-2d-grid.java
+++ 1663-detect-cycles-in-2d-grid/1663-detect-cycles-in-2d-grid.java
@@ -1,34 +1,29 @@
 class Solution {
-    private int[][] directions = {{1,0},{0,1},{-1,0},{0,-1}};
-    
-    private boolean dfs(int i, int j, int pi, int pj, char[][] grid, boolean[][] visited, char ch) {
-        visited[i][j] = true;
-
-        for (int[] dir : directions) {
-            int ni = i + dir[0];
-            int nj = j + dir[1];
-
-            if (ni >= 0 && ni < grid.length && nj >= 0 && nj < grid[0].length && grid[ni][nj] == ch) {
-                if (!visited[ni][nj]) {
-                    if (dfs(ni, nj, i, j, grid, visited, ch)) return true;
-                } else if (ni != pi || nj != pj) {
-                    // Found a visited cell that is not the parent → cycle!
-                    return true;
-                }
-            }
+    int m,n;
+    private int[][] directions = {{0,1},{1,0},{-1,0},{0,-1}};
+    private boolean dfs(char[][] grid,boolean[][] visited,int row,int col,int prow,int pcol){
+        visited[row][col] = true;
+        for(int[] dir : directions){
+          int nrow = dir[0] + row;
+          int ncol = dir[1] + col;
+          if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && grid[nrow][ncol] == grid[row][col]){
+           if (visited[nrow][ncol]) {
+                  if (nrow != prow || ncol != pcol) return true;
+              } else {
+                  if (dfs(grid, visited, nrow, ncol, row, col)) return true;
+             }
+          }
         }
         return false;
     }
-
     public boolean containsCycle(char[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        boolean[][] visited = new boolean[n][m];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!visited[i][j]) {
-                    if (dfs(i, j, -1, -1, grid, visited, grid[i][j])) {
+        m = grid.length;
+        n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j < n;j++){
+                if(!visited[i][j]){
+                    if(dfs(grid,visited,i,j,-1,-1) == true){
                         return true;
                     }
                 }
