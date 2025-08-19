0aa6ac02daebfbc791f75d89a92eff2a7edefd48
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-08-20 02:39:15 +0530

    Time: 7 ms (44.41%), Space: 45.2 MB (76.17%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,32 +1,30 @@
 class Solution {
-    private boolean dfs(List<List<Integer>> adj,boolean[] visited,boolean[] inRecursion,int curr){
-        visited[curr] = true;
-        inRecursion[curr] = true;
-        for(int v : adj.get(curr)){
-            if(visited[v] == false && !dfs(adj,visited,inRecursion,v)){
-                return false;
-            }else if(inRecursion[v] == true){
-                return false;
-            }
-        }
-        inRecursion[curr] = false;
-        return true;
-    }
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adj = new ArrayList<>();
-        for(int i = 0;i<numCourses;i++){
-            adj.add(new ArrayList<>());
-        }
+        for(int i = 0;i < numCourses;i++)adj.add(new ArrayList<>());
+        int[] indegree = new int[numCourses];
+        Arrays.fill(indegree,0);
         for(int[] arr : prerequisites){
-            adj.get(arr[1]).add(arr[0]);
+           adj.get(arr[1]).add(arr[0]);
+           indegree[arr[0]]++;
         }
-        boolean[] visited  = new boolean[numCourses];
-        boolean[] inRecusion = new boolean[numCourses];
-        for(int i = 0;i < numCourses;i++){
-            if(!visited[i] && !dfs(adj,visited,inRecusion,i)){
-               return false;
+        Queue<Integer> que = new LinkedList<>();
+        for(int i = 0;i<numCourses;i++){
+            if(indegree[i] == 0){
+                que.add(i);
             }
         }
-        return true;
+        List<Integer> ans = new ArrayList<>();
+        while(!que.isEmpty()){
+            int curr = que.poll();
+            ans.add(curr);
+            for(int v : adj.get(curr)){
+                indegree[v]--;
+                if(indegree[v] == 0){
+                    que.add(v);
+                }
+            }
+        }
+        return ans.size() == numCourses;
     }
 }
