8f676dc35654aec3916ff7328c0d58c927e9c97d
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-08-21 23:28:05 +0530

    Time: 17 ms (42.5%), Space: 57.4 MB (90%) - LeetHub

--- 1684-find-latest-group-of-size-m/1684-find-latest-group-of-size-m.java
+++ 1684-find-latest-group-of-size-m/1684-find-latest-group-of-size-m.java
@@ -1,28 +1,67 @@
 class Solution {
+    int[] parent, size;
+    
+    private int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+    
+    private void union(int x, int y, int[] count) {
+        int rootX = find(x);
+        int rootY = find(y);
+        
+        if (rootX == rootY) return;
+        
+        // merge smaller into larger
+        if (size[rootX] < size[rootY]) {
+            int temp = rootX;
+            rootX = rootY;
+            rootY = temp;
+        }
+        
+        // remove old group counts
+        count[size[rootX]]--;
+        count[size[rootY]]--;
+        
+        parent[rootY] = rootX;
+        size[rootX] += size[rootY];
+        
+        // add new merged group
+        count[size[rootX]]++;
+    }
+    
     public int findLatestStep(int[] arr, int m) {
         int n = arr.length;
-        if (m == n) return n; // whole array forms size m at the end
-
-        int[] len = new int[n + 2];  // length of group
+        if (n == m) return n;  // whole array is the group
+        
+        parent = new int[n + 2];
+        size = new int[n + 2];
+        boolean[] active = new boolean[n + 2];
+        int[] count = new int[n + 1];
+        
+        for (int i = 0; i < parent.length; i++) parent[i] = i;
+        
         int res = -1;
-        int count = 0;  // how many groups of size m exist
-
+        
         for (int step = 0; step < n; step++) {
-            int i = arr[step];
-            int left = len[i - 1];
-            int right = len[i + 1];
-            int total = left + right + 1;
-
-            // merge groups
-            len[i - left] = len[i + right] = total;
-
-            // update counts
-            if (left == m) count--;  
-            if (right == m) count--;  
-            if (total == m) count++;
-
-            if (count > 0) res = step + 1;
+            int pos = arr[step];
+            active[pos] = true;
+            parent[pos] = pos;
+            size[pos] = 1;
+            count[1]++;
+            
+            if (active[pos - 1]) {
+                union(pos, pos - 1, count);
+            }
+            if (active[pos + 1]) {
+                union(pos, pos + 1, count);
+            }
+            
+            if (count[m] > 0) res = step + 1;
         }
+        
         return res;
     }
 }
