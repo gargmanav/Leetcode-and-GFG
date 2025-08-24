dd55fd4cef7da776b89fa5157147ee99c0c6e6df
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-08-24 23:46:45 +0530

    Time: 135 ms (40.57%), Space: 45.2 MB (57.14%) - LeetHub

--- 3271-count-the-number-of-houses-at-a-certain-distance-i/3271-count-the-number-of-houses-at-a-certain-distance-i.java
+++ 3271-count-the-number-of-houses-at-a-certain-distance-i/3271-count-the-number-of-houses-at-a-certain-distance-i.java
@@ -1,16 +1,22 @@
 class Solution {
     public int[] countOfPairs(int n, int x, int y) {
-        int[] ans = new int[n];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i == j) continue;
-                int direct = Math.abs(i - j);
-                int viaShortcut = Math.min(Math.abs(i - x) + 1 + Math.abs(y - j),
-                                           Math.abs(i - y) + 1 + Math.abs(x - j));
-                int minDist = Math.min(direct, viaShortcut);
-                ans[minDist - 1]++;
+        int[][] dist = new int[n][n];
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(i == j) dist[i][j] = 0;
+                else if(Math.abs(i-j) == 1 || (i+1==x && j+1==y) || (i+1==y && j+1==x)) dist[i][j] = 1;
+                else dist[i][j] = 1000;
             }
         }
+        for(int k=0; k<n; k++)
+            for(int i=0; i<n; i++)
+                for(int j=0; j<n; j++)
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+        int[] ans = new int[n];
+        for(int i=0; i<n; i++)
+            for(int j=0; j<n; j++)
+                if(i != j && dist[i][j] < n)
+                    ans[dist[i][j]-1]++;
         return ans;
     }
 }
