584b872d10e532681964b3d94f67ebc057b925c7
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-09-02 03:06:47 +0530

    Time: 13 ms (84.93%), Space: 47.1 MB (73.81%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,27 +1,28 @@
 class Solution {
-    int m,n;
-    private final int[][] directions = {{0,1},{1,0},{0,-1},{-1,0}};
     public int[][] updateMatrix(int[][] mat) {
-        m = mat.length;
-        n = mat[0].length;
         Queue<int[]> que = new LinkedList<>();
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
+        int m = mat.length;
+        int n = mat[0].length;
+        for(int i = 0;i < m;i++){
+            for(int j = 0;j < n;j++){
                 if(mat[i][j] == 0){
                     que.add(new int[]{i,j});
                 }else{
-                    mat[i][j] = Integer.MAX_VALUE/2;
+                    mat[i][j] = Integer.MAX_VALUE;
                 }
             }
         }
+        int[][] directions = {{0,1},{1,0},{0,-1},{-1,0}};
         while(!que.isEmpty()){
-            int[] temp = que.poll();
+            int[] curr = que.poll();
             for(int[] dir : directions){
-                int nr = temp[0] + dir[0];
-                int nc = temp[1] + dir[1];
-                if(nr >= 0 && nr < m && nc >= 0 && nc < n && mat[nr][nc] > mat[temp[0]][temp[1]] + 1){
-                   mat[nr][nc] = mat[temp[0]][temp[1]] + 1;
-                   que.add(new int[]{nr,nc});
+                int nr = dir[0] + curr[0];
+                int nc = dir[1] + curr[1];
+                if(nr >= 0 && nr < m && nc >= 0 && nc < n){
+                    if(mat[nr][nc] > mat[curr[0]][curr[1]] + 1){
+                       mat[nr][nc] = mat[curr[0]][curr[1]] + 1;
+                       que.add(new int[]{nr,nc});
+                    }
                 }
             }
         }
