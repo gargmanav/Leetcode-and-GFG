35e6f32594710660202d28d39d8c4e262e1bd2ea
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-09-02 02:18:24 +0530

    Time: 1 ms (100%), Space: 42.1 MB (94.11%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,45 +1,40 @@
 class Solution {
     private int[][] directions = {{0,1},{1,0},{0,-1},{-1,0}};
-
     public int orangesRotting(int[][] grid) {
-        int freshOranges = 0;
-        int rottenOranges = 0;
-        int n = grid.length;
-        int m = grid[0].length;
+        int m = grid.length;
+        int n = grid[0].length;
+        int rotten = 0;
+        int fresh = 0;
         Queue<int[]> que = new LinkedList<>();
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(grid[i][j] == 1){
-                    freshOranges++;
-                }else if(grid[i][j] == 2){
-                    que.add(new int[]{i,j});
-                }
+        for(int i = 0;i < m;i++){
+            for(int j = 0;j < n;j++){
+               if(grid[i][j] == 2){
+                rotten++;
+                que.add(new int[]{i,j});
+               }else if(grid[i][j] == 1){
+                fresh++;
+               }
             }
         }
-       
-       int time = 0;
-       while(!que.isEmpty()){
-         int size = que.size();
-         boolean flag = false;
-         for(int i = 0;i < size;i++){
-            int[] arr = que.poll();
-            int a = arr[0];
-            int b = arr[1];
-            for(int[] newarr : directions){
-                int na = a + newarr[0];
-                int nb = b + newarr[1];
-                if(na >= 0 && na < n && nb >= 0 && nb < m && grid[na][nb] == 1){
-                    que.add(new int[]{na,nb});
-                    grid[na][nb] = 2;
-                    freshOranges--;
-                    flag = true;
+        if(fresh == 0)return 0;
+        int time = 0;
+        while(!que.isEmpty()){
+            int size = que.size();
+            if(fresh == 0)return time;
+            time++;
+            for(int i = 0;i < size;i++){
+              int[] curr = que.poll();
+              for(int[] dir : directions){
+                int nr = curr[0] + dir[0];
+                int nc = curr[1] + dir[1];
+                if(nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1){
+                    fresh--;
+                    grid[nr][nc] = 2;
+                    que.add(new int[]{nr,nc});
                 }
+              }
             }
-            
-         }
-         if(flag)time++;
+        }
+        return -1;
     }
-    return freshOranges == 0 ? time : -1;
-}
-
 }
