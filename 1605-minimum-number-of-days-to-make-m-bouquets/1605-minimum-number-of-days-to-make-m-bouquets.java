68282a6728670f5efe7cffab5eb9c255d19d7589
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-09-03 03:40:20 +0530

    Time: 23 ms (16.96%), Space: 59.7 MB (6.06%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,33 +1,32 @@
 class Solution {
-    private boolean checkpossible(int mid,int m,int k,int[] bloomDay){
+    private boolean checkday(int target,int[] bloomDay,int m,int k){
         int count = 0;
-        int bouq = 0;
-        for(int day : bloomDay){
-            if(day <= mid){
-                count++;
-                if(count == k){
-                    bouq++;
-                    count = 0;
+        int kcount = 0;
+        for(int i = 0;i < bloomDay.length;i++){
+            if(bloomDay[i] <= target){
+                kcount++;
+                if(kcount == k){
+                    kcount=0;
+                   count++;
                 }
-            }else{
-                count = 0;
-            }
+            }else kcount = 0;
         }
-        return bouq >= m;
+        return count >= m;
     }
     public int minDays(int[] bloomDay, int m, int k) {
         int n = bloomDay.length;
-        if(n < m*k)return -1;
-        int low = 1;
+        if(n < m * k)return -1;
+        int low = Arrays.stream(bloomDay).min().getAsInt();
         int high = Arrays.stream(bloomDay).max().getAsInt();
-        int ans = -1;
+        int minimumdays = Integer.MAX_VALUE;
         while(low <= high){
             int mid = low + (high - low)/2;
-            if(checkpossible(mid,m,k,bloomDay)){
-                ans = mid;
+            if(checkday(mid,bloomDay,m,k)){
+                minimumdays = Math.min(minimumdays,mid);
                 high = mid - 1;
-            }else low = mid + 1;
+            }
+            else low = mid + 1;
         }
-        return ans;
+        return minimumdays == Integer.MAX_VALUE ? -1 : minimumdays;
     }
 }
