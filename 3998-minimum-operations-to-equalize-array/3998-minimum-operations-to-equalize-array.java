c957e064e350fdb43c4254a863ed5db199e67d6d
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-09-10 02:48:56 +0530

    Time: 2 ms (10.36%), Space: 43.5 MB (71.52%) - LeetHub

--- 3998-minimum-operations-to-equalize-array/3998-minimum-operations-to-equalize-array.java
+++ 3998-minimum-operations-to-equalize-array/3998-minimum-operations-to-equalize-array.java
@@ -1,8 +1,7 @@
 class Solution {
     public int minOperations(int[] nums) {
         Arrays.sort(nums);
-        int n = nums.length;
-        if(nums[0] == nums[n - 1])return 0;
+        if(nums[0] == nums[nums.length - 1])return 0;
         return 1;
     }
 }
