9d9761a7fab1fadfc29554033955b0a2dc42b52e
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-09-28 14:39:52 +0530

    Time: 14 ms (5.49%), Space: 43.1 MB (7.01%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,35 +1,31 @@
 class Solution {
     public int myAtoi(String s) {
-        int i = 0, n = s.length();
+        int i = 0;
         int sign = 1;
-        long result = 0;
-
-        // 1. Skip leading whitespaces
-        while (i < n && s.charAt(i) == ' ') {
+        int n = s.length();
+        while(i < n && s.charAt(i) == ' '){
             i++;
         }
-
-        // 2. Handle optional '+' or '-' sign
-        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
-            sign = s.charAt(i) == '-' ? -1 : 1;
-            i++;
-        }
-
-        // 3. Convert digits
-        while (i < n && Character.isDigit(s.charAt(i))) {
-            int digit = s.charAt(i) - '0';
-            result = result * 10 + digit;
-
-            // 4. Clamp result to Integer range if it overflows
-            if (sign * result <= Integer.MIN_VALUE) {
-                return Integer.MIN_VALUE;
-            } else if (sign * result >= Integer.MAX_VALUE) {
-                return Integer.MAX_VALUE;
+        if(i < n && (s.charAt(i) == '-' || s.charAt(i) == '+')){
+            if(s.charAt(i) == '-'){
+               sign *= -1;
             }
-
+           i++;
+        }
+        if(i < n && s.charAt(i) == '0'){
             i++;
         }
-
-        return (int) (sign * result);
+        int result = 0;
+        while(i < n && Character.isDigit(s.charAt(i))){
+           int curr = s.charAt(i) - '0';
+           System.out.println(curr);
+           if(result > (Integer.MAX_VALUE - curr)/10){
+            return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+           }
+           result = result * 10 + curr;
+          
+           i++;
+        }
+        return  sign * result;
     }
 }
