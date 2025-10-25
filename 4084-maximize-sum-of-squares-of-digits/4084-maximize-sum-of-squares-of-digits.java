9d21c9b60892dd7ae7fdabba980f48ba5be11f3d
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-10-25 22:40:05 +0530

    Time: 123 ms (0%), Space: 46.3 MB (0%) - LeetHub

---
+++ 4084-maximize-sum-of-squares-of-digits/4084-maximize-sum-of-squares-of-digits.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String maxSumOfSquares(int num, int sum) {
+        if (sum > 9 * num) return "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < num; i++) {
+            int digit = Math.min(9, sum);
+            sb.append(digit);
+            sum -= digit;
+        }
+        return sum == 0 ? sb.toString() : "";
+    }
+}
