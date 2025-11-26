07b8b3a422956d9383126bbb1b8f16317edac7c9
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-11-26 13:33:06 +0530

    Time: 8 ms (50.58%), Space: 48.7 MB (36.96%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
@@ -1,16 +1,15 @@
 class Solution {
     public void wiggleSort(int[] nums) {
-        int n = nums.length;
-        int[] sorted = nums.clone();
-        Arrays.sort(sorted);
-
-        int left = (n - 1) / 2;
-        int right = n - 1;
-        for (int i = 1; i < n; i += 2) {
-            nums[i] = sorted[right--];
-        }
-        for (int i = 0; i < n; i += 2) {
-            nums[i] = sorted[left--];
-        }
+       int n = nums.length;
+       int[] sorted = nums.clone();
+       Arrays.sort(sorted);
+       int left = (n - 1)/2;
+       int right = n - 1;
+       for(int i = 1;i < n;i+=2){
+         nums[i] = sorted[right--];
+       }
+       for(int i = 0;i < n;i+=2){
+        nums[i] = sorted[left--];
+       }
     }
 }
