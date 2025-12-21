e0c7e6c64fa1aba3f65ed03884fcd9bf6de572eb
Manav garg <78503190+gargmanav@users.noreply.github.com>
2025-12-22 01:04:54 +0530

    Time: 3 ms (100%), Space: 105.2 MB (0%) - LeetHub

---
+++ 3784-minimum-deletion-cost-to-make-all-characters-equal/3784-minimum-deletion-cost-to-make-all-characters-equal.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long minCost(String s, int[] cost) {
+        long totalCost = 0;
+        long[] keep = new long[26]; // cost sum for each character
+
+        for (int i = 0; i < s.length(); i++) {
+            int idx = s.charAt(i) - 'a';
+            keep[idx] += cost[i];
+            totalCost += cost[i];
+        }
+
+        long maxKeep = 0;
+        for (long val : keep) {
+            maxKeep = Math.max(maxKeep, val);
+        }
+
+        return totalCost - maxKeep;
+    }
+}
