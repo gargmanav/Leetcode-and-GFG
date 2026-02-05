7c7a243388c86138dca2e1fcbbd6b369ab4dd7cf
Manav garg <78503190+gargmanav@users.noreply.github.com>
2026-02-05 22:02:40 +0530

    Time: 77 ms (50.22%), Space: 69.2 MB (61.82%) - LeetHub

---
+++ 3805-count-caesar-cipher-pairs/3805-count-caesar-cipher-pairs.java
@@ -0,0 +1,25 @@
+class Solution {
+    public long countPairs(String[] words) {
+        Map<String, Integer> map = new HashMap<>();
+
+for (String word : words) {
+    int shift = word.charAt(0) - 'a';
+    StringBuilder sb = new StringBuilder();
+
+    for (char c : word.toCharArray()) {
+        char norm = (char)((c - shift + 26 - 'a') % 26 + 'a');
+        sb.append(norm);
+    }
+
+    map.put(sb.toString(), map.getOrDefault(sb.toString(), 0) + 1);
+}
+
+long ans = 0;
+for (int count : map.values()) {
+    ans += (long) count * (count - 1) / 2;
+}
+
+return  ans;
+
+    }
+}
